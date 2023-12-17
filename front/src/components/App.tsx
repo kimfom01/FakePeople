5d2217d3a63fa0fb07c1d51bc65f4e3e853d0809
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-17 16:45:44 +0300

    Move loading logic to get people

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,20 +1,8 @@
-import { Suspense } from "react";
 import { Base } from "./Base";
 import { GetPeople } from "./GetPeople";
-import { ErrorBoundary } from "react-error-boundary";
 
 const App = () => {
-  return (
-    <Base
-      child={
-        <ErrorBoundary fallback={<div>Error! Something went wrong</div>}>
-          <Suspense fallback={"Loading..."}>
-            <GetPeople />
-          </Suspense>
-        </ErrorBoundary>
-      }
-    />
-  );
+  return <Base child={<GetPeople />} />;
 };
 
 export default App;
