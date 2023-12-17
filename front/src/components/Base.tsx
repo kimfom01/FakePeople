ae78e81dac617ecc5620307e68230315acaaf490
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-17 12:37:46 +0300

    put container div at base level

--- front/src/components/Base.tsx
+++ front/src/components/Base.tsx
@@ -1,13 +1,25 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 interface Prop {
   Child: ReactElement;
 }
 
 export const Base = ({ Child }: Prop) => {
+  // const [errorState, setErrorState] = useState<string>("");
+
   return (
-    <div className="App">
-      <main className="App-main">{Child}</main>
-    </div>
+    <>
+      <nav className="navbar bg-dark fixed-top">
+        <div className="container">
+          <Link to="/" className="navbar-brand text-light">
+            Fake People
+          </Link>
+        </div>
+      </nav>
+      <main>
+        <div className="container">{Child}</div>
+      </main>
+    </>
   );
 };
