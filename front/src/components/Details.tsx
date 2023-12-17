12b26b84a47405dbc3daf789f8a458421d1cf98e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-17 15:15:30 +0300

    style details page

--- front/src/components/Details.tsx
+++ front/src/components/Details.tsx
@@ -1,5 +1,16 @@
 import { Base } from "./Base";
 
 export const Details = () => {
-  return <Base Child={<div>Hello World</div>} />;
+  return (
+    <Base
+      child={
+        <div
+          style={{ height: "calc(100vh - 56px)" }}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <div className="mt-3 h1">This is the details page</div>
+        </div>
+      }
+    />
+  );
 };
