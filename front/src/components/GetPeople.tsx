4b66aab525ca95a5cdbae28817c865196629052c
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-17 12:37:27 +0300

    Remove container div

--- front/src/components/GetPeople.tsx
+++ front/src/components/GetPeople.tsx
@@ -16,35 +16,33 @@ export const GetPeople = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="row g-0 justify-content-center">
-          {people?.map((person, key) => (
-            <div
-              key={key}
-              className="card-frame card col-sm-12 col-md-3 col-lg-2 m-3"
-            >
-              <Link to="/details" className="link-dark text-decoration-none">
-                <div className="text-decoration-none">
-                  <div className="card-body">
-                    <img
-                      src={person.imageUrl}
-                      alt="Some random photograph"
-                      style={{ width: "100%" }}
-                    />
-                    <hr />
-                    <div className="card-title">
-                      <p>
-                        {person.firstName} {person.lastName}
-                      </p>
-                      Age: {person.age}
-                    </div>
-                    Bio: <div className="text-truncate">{person.bio}</div>
+      <div className="row g-0 justify-content-center">
+        {people?.map((person, key) => (
+          <div
+            key={key}
+            className="card-frame card col-sm-12 col-md-3 col-lg-2 m-3"
+          >
+            <Link to="/details" className="link-dark text-decoration-none">
+              <div className="text-decoration-none">
+                <div className="card-body">
+                  <img
+                    src={person.imageUrl}
+                    alt="Some random photograph"
+                    style={{ width: "100%" }}
+                  />
+                  <hr />
+                  <div className="card-title">
+                    <p>
+                      {person.firstName} {person.lastName}
+                    </p>
+                    Age: {person.age}
                   </div>
+                  Bio: <div className="text-truncate">{person.bio}</div>
                 </div>
-              </Link>
-            </div>
-          ))}
-        </div>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </>
   );
