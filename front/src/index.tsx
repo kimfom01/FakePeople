33b7059d4361c4129d5750fe938220966b5b4563
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-17 10:47:57 +0300

    Setup router

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Details } from "./components/Details";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <App />,
+  },
+  {
+    path: "/details",
+    element: <Details />,
+    errorElement: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
